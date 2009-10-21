b706ec4245d22401d2b61dc332de430ba1caedd7
AlekMin88 <AlekMin88@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2009-10-21 19:14:34 +0000

    made MemVal class and MemoryModel class

--- trunk/sources/mineev/test/test/memory.cpp
+++ trunk/sources/mineev/test/test/memory.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <cassert>
+#include <math.h>
 #ifndef MEMORY_HEADER
 #define MEMORY_HEADER
 #include "memory.h"
@@ -15,6 +16,18 @@
 /**
  * Implementation of class ByteLine
  */
+ByteLine::ByteLine()
+{
+	byte_line = new vector<Byte>;
+}
+
+ByteLine::ByteLine( unsigned int count)
+{
+	byte_line = new vector<Byte>;
+	//(*byte_line).reserve( count);
+	(*byte_line).resize( count);
+}
+
 ByteLine::ByteLine( const ByteLine& line)
 {
     try
@@ -116,5 +129,219 @@ void ByteLine::addByte( const Byte& byte)
         assert( 0);
     }
 }
+void ByteLine::resizeByteLine( unsigned int count)
+{
+	try
+    {
+		( *byte_line).resize( count);
+	}catch ( std::bad_alloc)
+    {
+        cout << "ERROR: Can not allocate memory!\n";
+        assert( 0);
+    }
+}
 
+
+/**
+ * Implementation of class MemVal
+ */
+
+MemVal::MemVal( const ByteLine& line, unsigned int size):ByteLine( line),
+                                                                size_of_segmentation( size)
+{
+    recountLenght();
+}
+
+void MemVal::recountLenght()
+{
+    unsigned int temp = getSizeOfMemVal() % size_of_segmentation;
+    if ( temp != 0)
+    {
+        temp = size_of_segmentation - temp;
+        temp += getSizeOfMemVal();
+        resizeByteLine( temp);
+    }
+}
+void MemVal::resizeMemVal( unsigned int size)
+{
+    resizeByteLine( size);
+    recountLenght();
+}
+
+
+ByteLine MemVal::getByteLine( unsigned int index, unsigned int count) const
+{
+    if ( getSizeOfMemVal() < index + count)
+    {
+        cout << "ERROR: Size of MemVal is less than target byte number!\n";
+        assert( 0);
+    }
+    ByteLine temp( count);
+    for ( unsigned int i = 0; i < count ; i++)
+    {
+        temp.setByte( i, getByte( i + index));
+    }
+    return temp;
+}
+ByteLine MemVal::getByteLine() const
+{
+    ByteLine temp( getSizeOfMemVal());
+    for ( unsigned int i = 0; i < getSizeOfMemVal(); i++)
+    {
+        temp.setByte( i, getByte( i));
+    }
+    return temp;
+}
+
+void MemVal::writeByteLine( const ByteLine& line, unsigned int index)
+{
+    if ( getSizeOfMemVal() < index + line.getSizeOfLine())
+    {
+        cout << "ERROR: Size of MemVal is less than target byte number!\n";
+        assert( 0);
+    }
+    for ( unsigned int i = 0; i < line.getSizeOfLine(); i++)
+    {
+        setByte( i + index, line.getByte( i));
+    }
+}
+void MemVal::writeByteLine( const ByteLine & line)
+{
+    if ( getSizeOfMemVal() < line.getSizeOfLine())
+    {
+        cout << "ERROR: Size of MemVal is less than target byte number!\n";
+        assert( 0);
+    }
+    for ( unsigned int i = 0; i < line.getSizeOfLine(); i++)
+    {
+        setByte( i, line.getByte( i));
+    }
+}
+
+
+
+
+/**
+ * Implementation of Memory Model
+ */
+/*
+MemoryModel::MemoryModel( unsigned int size_of_segmentation)
+{
+	mem_model = new map<MemVal,mathAddr>;
+	
+    //(*mem_model).second = getMathAddr();
+}
+
+
+MemVal MemoryModel::read(mathAddr read_ptr, unsigned int num_of_bytes)
+{
+	if( ( *mem_model).empty())
+	{
+		//error
+	}
+	
+	map< MemVal, mathAddr>::iterator pos, start, end;
+	start = find( read_ptr);	
+	//mathAddr temp_adrr = start->second;
+	end = find( read_ptr + start->second);
+	if( start == ( *mem_model).end() || end == ( *mem_model).end())
+	{
+		//erorr
+	}
+	MemVal temp = start->first;
+	for ( pos = start; pos != end; ++pos)
+	{
+		if ( countDistance( pos) > 1)
+		{
+			//erorr
+		}
+		mergeMemVal( pos, temp);
+		(*mem_model).erase( pos);
+	}
+	(*mem_model)[ temp] = start->second;
+	return temp;	
+}
+
+				
+			
+void MemoryModel::mergeMemVal( map<MemVal,mathAddr>::iterator pos, MemVal mem_val)
+{
+	if( countDistance(pos) > 1)
+	{
+			mem_val.resizeMemVal( (++pos)->second - pos->second - 1 );
+	} 
+		mem_val = mem_val + (++pos)->first;	
+}
+
+*/
+/*
+void MemoryModel::write(mathAddr write_ptr, ByteLine line)
+{
+	map< MemVal, mathAddr>::iterator pos,start,end ;
+	start = findOrInit( write_ptr);
+	end = findOrInit( write_ptr + start->second);
+
+	MemVal memval = start->first;
+	for ( pos = start; pos != end; ++pos)
+	{
+		mergeMemVal( pos, memval);
+		( *mem_model).erase( pos);
+	}
+	memval.writeByteLine( line, write_ptr - start->second);
+	(*mem_model)[memval] = start->second;
+}
+*/
+
+
+
+	
+/*
+map< MemVal, mathAddr>::iterator MemoryModel::findOrInit(mathAddr ptr)
+{
+	map< MemVal, mathAddr>::iterator pos;
+	mathAddr adrr;
+	
+	MemVal temp( size_of_segmentation); 
+	adrr = ptr - (ptr%size_of_segmentation);
+	for ( pos = (*mem_model).begin(); pos != (*mem_model).end(); ++pos)
+	{
+		if ( pos == ptr)
+		{
+			return pos;
+		}
+		if ( (pos->second) > ptr)
+		{
+			//pair< MemVal, mathAddr> p( temp, adrr);
+			(*mem_model)[temp] = adrr;
+			pos = (*mem_model).find(temp);
+			return pos;
+			
+		}
+	}
+	return pos = (*mem_model).begin();
+}
+*/
+/*
+map< MemVal, mathAddr>::iterator MemoryModel::find( mathAddr ptr)
+{
+	map< MemVal, mathAddr>::iterator pos;
+	mathAddr adrr;
+	adrr = ptr - (ptr%size_of_segmentation);
+	for ( pos = (*mem_model).begin(); pos != (*mem_model).end(); ++pos)
+	{
+		if ( pos == ptr)
+		{
+			return pos;
+		}
+	}
+	return pos = (*mem_model).end();
+}
+
+unsigned int MemoryModel::countDistance( map< MemVal, mathAddr>::iterator pos)
+{
+	return (++pos)->second - (pos->second + (pos->first).getSizeOfMemVal());
+}
+
+			
+*/
 
