ea7eb0e1e47fc328afc76093d62b45f1f07f6c91
dmitrikorobov <dmitrikorobov@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2009-10-28 19:37:13 +0000

    I've added my files to funcsim (register_file.h, register_file.cpp)

--- trunk/sources/funcsim/register_file.h
+++ trunk/sources/funcsim/register_file.h
@@ -13,44 +13,37 @@
 #include "memory.h"
 
 /**
- * class RegVal implements a object to interaction with RF
+ * class RegVal implements a object to interaction with RF 
  */
 
 class RegVal: public ByteLine
 {
-    ByteLine* byte_line;
-
+  
 public:
-    RegVal() {};
     /* Constructors and destructor */
-    RegVal( unsigned int number_of_bytes_in_reg_val)
+    RegVal( unsigned int size_in_bytes): ByteLine( size_in_bytes)
     {
-       byte_line = new ByteLine( number_of_bytes_in_reg_val);
-       if ( !byte_line)
-       {
-            cout << "ERROR: Can not allocate memory!\n";
-            assert(0);
-        }
     }
+
     ~RegVal()
     {
-        delete byte_line;
     }
 
-    /* Get/set methods */
-    hostUInt8 getByteVal( unsigned int byte_num)
+    RegVal& operator =( RegVal& reg_val)
     {
-        return byte_line->getByteVal(byte_num);
-    }
+        if ( this->getSizeOfLine() != reg_val.getSizeOfLine())
+	    {
+		    cout << "Error: you can write to RegVal with the equal length only!\n";
+		    assert( 0);
+        }
+        int sz_of_ln = this->getSizeOfLine();
 
-    void setByteVal( unsigned int byte_num, hostUInt8 byte_val)
-    {
-        byte_line->setByte(byte_num, byte_val);
-    }
+        for ( int i = 0; i < sz_of_ln; i++)
+        {
+            this->setByteVal( i, reg_val.getByteVal( i));
+        }
 
-    unsigned int getSizeOfMemVal()
-    {
-        return this->byte_line->getSizeOfLine();
+        return ( *this);
     }
 };
 
@@ -58,22 +51,22 @@ public:
  * class RegisterFileModel implements RF of simulated architecture and infrastructure to operate with it
  */
 
-class RegisterFileModel
+class RegisterFileModel 
 {
+    RegVal** reg_file;          // Array of RegVal*
+    unsigned int num_of_reg;    // Number of registers in the register file
 
 public:
-    /* Constructors and destructor
+	
+    /* Constructors and destructor */
     RegisterFileModel( unsigned int number_of_registers, unsigned int size_of_register_in_bytes);
     ~RegisterFileModel();
-     */
-
-    /* Read a logical set of bytes (RegVal) form physical register with number reg_num
+     
+    /* Read a logical set of bytes (RegVal) form physical register with number reg_num */
     RegVal* readReg( physRegNum reg_num);
-     */
-
-    /* Write a logical set of bytes (RegVal) to physical register with number reg_num
-    void writeReg( physRegNum reg_num, RegVal reg_value);
-     */
+     
+    /* Write a logical set of bytes (RegVal) to physical register with number reg_num */
+    void writeReg( physRegNum reg_num, RegVal& reg_value);
 };
 
 #endif /* REGISTER_FILE_H */
