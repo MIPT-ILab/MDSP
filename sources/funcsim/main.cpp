c6f60455447d0cdb2ba0d1604f74945a5a48d607
dmitrikorobov <dmitrikorobov@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2009-10-29 09:18:41 +0000

    I've added unlinking main to funcsim

--- sources/funcsim/main.cpp
+++ sources/funcsim/main.cpp
@@ -6,6 +6,7 @@
 #include "operation.h"
 #include "test.h"
 #include "core.h"
+#include "register_file.h"
 
 int main()
 {
@@ -13,7 +14,24 @@ int main()
     core->init( 0x0000);
     core->run();
 
+    RegisterFileModel* rfm = new RegisterFileModel( 2, 1);
+    RegVal* rv = new RegVal( 1);
+    RegVal rv1( 1);
+    Byte* b1 = new Byte( 5);
+    Byte* b2 = new Byte( 2);
+    rv->setByte( 0, *b1);
+    rv1.setByte( 0, *b2);
+    rfm->writeReg( 1, *rv);
+    rfm->writeReg( 0, rv1);
+
+    cout << ( int)rfm->readReg( 0)->getByte( 0).getByteVal() << " " <<
+            ( int)rfm->readReg( 1)->getByte( 0).getByteVal() << "\n"; 
+    delete b1;
+    delete b2;
+    delete rv;
+    delete rfm;
+
     //test();
 
-    return 1;
+    return 0;
 }
