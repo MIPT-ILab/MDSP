04590836506ee34384c5f7c87a88e5285b19fcf3
aspotashev <aspotashev@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2009-11-13 10:51:42 +0000

    Define isblank for libc versions not supporting it

--- sources/aspotashev/as/ident.h
+++ sources/aspotashev/as/ident.h
@@ -1,3 +1,8 @@
 
 #define isidstart(c) ((c) == '_' || isalpha(c))
 #define isidchar(c)  (isidstart(c) || isdigit(c))
+
+#if not (_XOPEN_SOURCE >= 600 || _ISOC99_SOURCE)
+#define isblank(c) ((c) == ' ' || (c) == '\t')
+#endif
+
