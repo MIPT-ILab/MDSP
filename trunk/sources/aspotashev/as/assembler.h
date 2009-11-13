78e66285bc52682f93e5967104d37d91d9150805
aspotashev <aspotashev@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2009-11-13 09:02:58 +0000

    Add comments to the sources of assembler

--- trunk/sources/aspotashev/as/assembler.h
+++ trunk/sources/aspotashev/as/assembler.h
@@ -5,15 +5,42 @@
 #include "semantican.h"
 
 
+/**
+ * This class is used to assemble an executable image a sequence of
+ * semantic units
+ */
 class Assembler
 {
 public:
+    /**
+     * Convenience constructor
+     *
+     * @param units The sequence of semantic units to work with.
+     */
     Assembler( std::vector<SemanticAn::Unit *> units);
+
+    /**
+     * Generates an executable image defined by a list of
+     * pairs (byte address; byte value)
+     */
     std::map<unsigned int, hostUInt8> run();
+
+    /**
+     * Encodes a processor instruction located at word address @pc
+     * in program memory
+     *
+     * @param operation Pointer to the assembler command.
+     * @param pc The word address of the instruction in program memory.
+     */
     ByteLine *encodeOperation(
         SemanticAn::Unit *operation, unsigned int pc);
 
 private:
+    /**
+     * @internal
+     * Return the number of a register define by an identifier, e.g.
+     * "%r23" => 23
+     */
     int getGprNum( std::string id);
 
 private:
--- trunk/sources/aspotashev/as/semantican.h
+++ trunk/sources/aspotashev/as/semantican.h
@@ -20,6 +20,11 @@ enum OPERAND_TYPE
     OPERAND_CUSTOM_ID,
 };
 
+
+/**
+ * This class is used to generate a sequence of semantic unit from a
+ * sequence of tokens
+ */
 class SemanticAn
 {
 public:
@@ -30,15 +35,53 @@ public:
         std::string sVal;
         int iVal;
 
+        /**
+         * Default constructor
+         *
+         * Sets @indirect to false
+         */
         Operand();
-        static Operand *createId( std::string id_string);
-        static Operand *createIdInd( std::string id_string);
-        static Operand *createConstInt( int iVal);
-        void dump();
 
+        /**
+         * Creates an operand defined by a single identifier
+         *
+         * @param id_string The identifier in the operand.
+         */
+        static Operand *createId( std::string id_string);
+
+        /**
+         * Creates an indirect operand with the address in memory defined
+         * by an identifier. Example: "(%r0)"
+         *
+         * @param id_string The identifier in the operand.
+         */
+        static Operand *createIdInd( std::string id_string);
+
+        /**
+         * Creates a constant integer operand
+         *
+         * @param iVal The value of the constant.
+         */
+        static Operand *createConstInt( int iVal);
+
+        /**
+         * Returns whether the operand is a memory address in a register
+         * ("(%r0)".."(%r31)")
+         */
         bool isIndirectGpr();
+
+        /**
+         * Returns whether the operand is a register
+         * ("%r0".."%r31")
+         */
         bool isDirectGpr();
+
+        /**
+         * Returns whether the operand is a constant integer
+         */
         bool isConstInt();
+
+        void dump();
     };
 
     struct Unit
@@ -48,23 +91,64 @@ public:
         std::string sVal;
         std::vector<Operand *> operands;
 
+        /**
+         * Creates a processor instruction as a semantic unit
+         *
+         * @param opcode The instruction name.
+         * @param operands The list of operands of the instruction.
+         */
         static Unit *createOperation(
             std::string opcode, std::vector<Operand *> operands);
+
+        /**
+         * Creates a label as a semantic unit
+         *
+         * @param id The identifier of the label.
+         */
         static Unit *createLabel( std::string id);
+
         void dump();
     };
 
 public:
+    /**
+     * Convenience constructor
+     *
+     * @param tokens The sequence of tokens to work with.
+     */
     SemanticAn( std::vector<TokenAn::Token *> tokens);
+
+    /**
+     * Generates the sequence of semantic units
+     */
     std::vector<Unit *> run();
 
 private:
-    bool isOpcode( std::string s); // arch
+    bool isOpcode( std::string s); // architecture-dependent
+
+    /**
+     * @internal
+     * Parses a sequence of tokens starting from @tok as an operands list
+     * split by commas and ending with an end-of-string, e.g.:
+     *      %r0, %r1, (%r2)\n
+     */
     std::vector<Operand *> parseOperandList();
+
+    /**
+     * @internal
+     * Parses a single assembler command operand
+     */
     Operand *parseOperand();
 
 private:
+    /**
+     * The list of tokens on the input
+     */
     std::vector<TokenAn::Token *> tokens;
+
+    /**
+     * Iterator pointing to the current token
+     */
     std::vector<TokenAn::Token *>::iterator tok;
 };
 
