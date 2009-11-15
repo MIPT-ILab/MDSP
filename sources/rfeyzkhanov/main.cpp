be41a6e4f020c28a8a9921c5314e1cda534c4a05
ryfeus <ryfeus@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2009-11-15 08:52:46 +0000

    Add example of usage byteline shift (in main)

--- sources/rfeyzkhanov/main.cpp
+++ sources/rfeyzkhanov/main.cpp
@@ -7,7 +7,21 @@
 
 int main()
 {
+    Byte a( 31);
+    Byte b( 63);
+    Byte c( 0);
+    cout<<"test byteline class:"<<endl; 
+    cout<<"a: "<<'\t'<<a<<endl;
+    cout<<"b: "<<'\t'<<b<<endl;
+    cout<<"c: "<<'\t'<<c<<endl;
 
+    ByteLine f( a + b + c);
+    ByteLine byteline = f;
+    cout<<(byteline[0].getByteVal()+byteline[1].getByteVal()*256+byteline[2].getByteVal()*65536)<<endl;
+    cout<<"byteline: "<<'\t'<<byteline<<endl;
+    byteline = byteline << 10;
+    cout<<(byteline[0].getByteVal()+byteline[1].getByteVal()*256+byteline[2].getByteVal()*65536)<<endl;
+    cout<<"byteline: "<<'\t'<<byteline<<endl;
     return 0;
 }
 
