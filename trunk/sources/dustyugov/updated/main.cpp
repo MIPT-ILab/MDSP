374438b36fe99a1db035bc49e45d864f19b9fbfe
dmitry.ustyugov <dmitry.ustyugov@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2009-11-17 20:13:20 +0000

    added files in order to debug

---
+++ trunk/sources/dustyugov/updated/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * main.cpp - Entry point of functional simulator (funcsim)
+ * Copyright 2009 MDSP team
+ */
+
+#include "core.h"
+#include "test.h"
+
+int main()
+{
+    Core* core = new Core();
+    core->init( 0x0000);
+    core->run();
+
+    //test();
+
+    return 0;
+}
