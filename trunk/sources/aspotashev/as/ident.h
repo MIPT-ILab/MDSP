8f2326f37739bee95997d5c1df8c04524dd17e96
aspotashev <aspotashev@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2009-11-17 18:31:36 +0000

    Always define isblank

--- trunk/sources/aspotashev/as/ident.h
+++ trunk/sources/aspotashev/as/ident.h
@@ -1,8 +1,5 @@
 
 #define isidstart(c) ((c) == '_' || isalpha(c))
 #define isidchar(c)  (isidstart(c) || isdigit(c))
-
-#if not (_XOPEN_SOURCE >= 600 || _ISOC99_SOURCE)
-#define isblank(c) ((c) == ' ' || (c) == '\t')
-#endif
+#define isblank(c) ((c) == ' ' || (c) == '\t') // Don't rely on libc
 
