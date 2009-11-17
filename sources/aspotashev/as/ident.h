b347acad6c5827250013199b001a3c7055529f1d
aspotashev <aspotashev@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2009-11-17 20:42:38 +0000

    Add more comments

--- sources/aspotashev/as/ident.h
+++ sources/aspotashev/as/ident.h
@@ -1,5 +1,16 @@
 
+/**
+ * Returns whether a character may be the first character of an identifier
+ */
 #define isidstart(c) ((c) == '_' || isalpha(c))
+
+/**
+ * Returns whether a character may be a character in an identifier
+ */
 #define isidchar(c)  (isidstart(c) || isdigit(c))
+
+/**
+ * Returns whether a character is a whitespace character
+ */
 #define isblank(c) ((c) == ' ' || (c) == '\t') // Don't rely on libc
 
