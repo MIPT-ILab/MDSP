aa24d7e10c0e487e2561214be8bcac0bbdf08255
WTertius <WTertius@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2010-04-05 13:28:11 +0000

    Автор: Дмитрий Лукиянчук.
    Добавлен файл handle_args.h;
    Изменен файл main.cpp - вложен добавленный файл, добавлены две переменные в функцию main - test_file и log_name - и функция обработки входных аргуметнов handleArgs. Описание работы функции в добавленном файле handle_args.h.

---
+++ sources/funcsim/handle_args.h
@@ -0,0 +1,172 @@
+/**
+ * handle_args.h - Analysing and handling of inputed arguments 
+ * @author Dmitry Lukiyanchuk
+ * Copyright 2009 MDSP team
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+
+const int file_name_size = 80;
+
+bool check_file_existing(const char* file_name);
+bool check_symbols(const char* file_name);
+
+/*
+* Function analises and handles inserted arguments.
+* There are three cases of corect call:
+*	funcsim -h
+*	funcsim <testname>
+*	funcsim <testname> -l <log>
+* Function saves <testname> into variable "test_file"
+*               <log> into variable "log_file"
+* Function checks inputed <testname> for existance.
+*	If <testname> doesn't exist,
+*	function will report about error and stop programm.
+* Function checks inputed <log> for acceptability of its symbols.
+*	Now allowed symbols are {a-z, A-Z, 0-9, _, .}.
+* If <log> already exist, function will ask user for replacing.
+* Any programmist can test if <log> was called.
+*	He should only check if variable log_name != "\0".
+*/
+int handleArgs(int argc, char** argv, char* test_file, char* log_file)
+{
+	char* version_number = "0.1";
+
+	try
+	{
+		/* case: funcsim -h */
+		if(argc == 2 && argv[1][0] == '-' && argv[1][1] == 'h' && argv[1][2] == '\0')
+		{
+			// Print help information and stop programm
+			cout << "Functional simulator of multimedia digital signal processor." << endl;
+			cout << "Version: " << version_number << endl;
+			cout << "Usage:" << endl;
+			cout << endl;
+			cout << "\t" << argv[0] << " -h - show this help" << endl;
+			cout << "\t" << argv[0] << " <testname> [options]" << endl;
+			cout << endl;
+			cout << "Available options:" << endl;
+			cout << "\t" << "-l <log> - print logs into file <log>" << endl;
+			return 1;    // Stop programm
+		}
+		/* case: funcsim <testname> */
+		else if(argc == 2)
+		{
+			/*
+			* Try if test_file exist,
+			* if not - report about error and stop programm;
+			* if yes - save test_file_name.
+			*/
+			if(check_file_existing(argv[1]) != 0)
+			{
+				cout << "Unable to open \"" << argv[1] << "\"" << endl;
+				return 2;    // Stop programm
+			}
+			strcpy_s(test_file, file_name_size, argv[1]);
+			return 0; // continue main
+		}
+		/* case: funcsim <testname> -l <log> */
+		else if(argc == 4 && argv[2][0] == '-' && argv[2][1] == 'l' && argv[2][2] == '\0')
+		{
+			/*
+			* Try if test_file exist,
+			* if not - report about error and stop programm;
+			* if yes - save test_file_name.
+			*/
+			if(check_file_existing(argv[1]) != 0)
+			{
+				cout << "Unable to open \"" << argv[1] << "\"" << endl;
+				return 2;    // Stop programm
+			}
+			strcpy_s(test_file, file_name_size, argv[1]);
+			if(check_symbols(argv[3]) != 0)
+			{
+				cout << "There is permitted symbol in log_file name." << endl;
+				return 3;
+			}
+			
+			if(check_file_existing(argv[3]) == 0)
+			{
+				cout << "File \"" << argv[3] << "\" already exist." 
+					 << "Do you wont replace it?" << " (y/n)" << endl;
+				char answer[10] = "\0";
+				bool flag = 1;
+				while(flag != 0)
+				{
+					cout << ">";
+					cin >> answer;
+					if(answer[0] == 'y' && answer[1] == '\0')
+					{
+						strcpy_s(log_file, file_name_size, argv[3]);
+						flag = 0;
+					}
+					else if(answer[0] == 'n' && answer[1] == '\0')
+					{
+						return 4;    // Stop programm
+					}
+					else
+					{
+						cout << "Incorrect input. Please answer again: (y/n)" << endl;
+					}
+				}
+			}
+			else
+			{
+				strcpy_s(log_file, file_name_size, argv[3]);
+			}
+			return 0; // continue main
+		}
+		else
+		{
+			cout << "Wrong input. Call \"" << argv[0] << " -h\" for help" << endl;
+			return 1;    // Stop programm
+		}
+	}
+	catch(...)
+	{
+		cout << "Something going wrong in handle_args!" << endl;
+		return 1;    // Stop programm
+	}
+	return 0;
+}
+
+bool check_file_existing(const char* file_name)
+{
+	ifstream fin(file_name);
+	if(!fin)
+	{
+		return 1;
+	}
+	fin.close();
+	return 0;
+}
+
+/*
+* Check acceptability of file name symbols
+* {a-z, A-Z, 0-9, _, .}.
+*/
+bool check_symbols(const char* file_name)
+{
+	unsigned short int len = int(strlen(file_name));
+	for(int i = 0; i < len; i++)
+	{
+		if((file_name[i] >= 'a' && file_name[i] <= 'z') 
+			|| (file_name[i] >= 'A' && file_name[i] <= 'Z') 
+			|| (file_name[i] >= '0' && file_name[i] <= '9')
+			|| file_name[i] == '_' || file_name[i] == '.')
+		{
+		}
+		else
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
