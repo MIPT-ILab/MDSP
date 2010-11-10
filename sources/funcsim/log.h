c274e051d46f6b00a9376b6a0ba66a724deebd32
El.StaS <El.StaS@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2010-11-10 12:16:05 +0000

    Merged with /selizarov

---
+++ sources/funcsim/log.h
@@ -0,0 +1,28 @@
+/**
+ * log.h - Header of log class 
+ * includes 2 methods to show warrnings and errors
+ * @author Stas Elizarov
+ * Copyright 2010 MDSP team
+ */
+
+#ifndef	LOG_H
+#define LOG_H
+
+#include <iostream>
+#include <cassert>
+#include <stdarg.h>
+
+using namespace std;
+
+class log
+{
+public:
+	/* Method to show warning message*/
+	void warning(const char * message, ...) const;
+
+	/* Method to show error and assert simulator*/
+	void critical(const char * message, ...) const;
+};
+
+
+#endif /* LOG_H */
