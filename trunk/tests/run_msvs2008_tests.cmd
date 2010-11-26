c629991f7bdb2be2647d2b1c32042ba4955808de
grigory.rechistov <grigory.rechistov@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2010-11-26 19:33:40 +0000

    configured MSVS2008 test script

--- trunk/tests/run_msvs2008_tests.cmd
+++ trunk/tests/run_msvs2008_tests.cmd
@@ -1,139 +1,115 @@
 @echo off
-rem This script use MS VS 2008 to build the MDSP.
-echo ===========================================
-echo This is experimental script!
-echo 
-echo ===========================================
+rem This script use MS VS 2008 to build and test the MDSP project.
 
-rem We still need CygWin for Unix utils
+REM ----------------------------------------------------------------------------
+REM "THE BEER-WARE LICENSE" (Revision 42):
+REM Grigory Rechistov wrote this file. As long as you retain this notice you
+REM can do whatever you want with this stuff. If we meet some day, and you think
+REM this stuff is worth it, you can buy me a beer in return.
+REM ----------------------------------------------------------------------------
+
+
+set VERSION=W2
+rem We still need CygWin for Unix utils, also sendEmail.exe
 PATH=c:\cygwin\bin;c:\buildbot\bin;%PATH%
+
+rem current date and time
+for /f %%x in ('C:\cygwin\bin\date.exe +%%Y-%%m-%%d-%%H-%%M-%%S') do set DATE=%%x
+
+rem set host name and type
+REM for /f %%x in ('c:\cygwin\bin\hostname.exe') do set HOST=%%x
+set HOST=%COMPUTERNAME% %OS% %PROCESSOR_ARCHITECTURE% 
 
 rem make CygWin happy with paths
 set CYGWIN=nodosfilewarning %CYGWIN%
 
-rem Set up the VS environment. 
-call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+rem Set up the MS VS environment. 
+rem call twice in case some users have different setup
+call "C:\Program Files\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
+call "%VS90COMNTOOLS%\vsvars32.bat"
 
-set HOMEPATH=c:\buildbot\mdsp-tests\vc2008
-set LOGFILE=output.txt
-
-rem TODO set DATETIME properly
-rem set DATETIME=date-time
+set HOMEPATH="c:\buildbot\mdsp-tests\%DATE%"
 
 echo Creating the working directory
 set WRK="%HOMEPATH%%
 
-rm -rf %WRK%
+REM rm -rf %WRK%
 mkdir %WRK% 
  
 cd %WRK%
-touch %LOGFILE%
- 
-rem logprint "MDSP test suite version $VERSION, started at $DATE"   
-rem logprint "The machine: $HOST" 
+
+REM set LOGFILE=output.txt
+REM touch %LOGFILE%
+
+echo ===============================================================
+echo MDSP test suite version %VERSION%, started at %DATE%
+echo The machine: %HOST% 
+echo ===============================================================
+set PASS=0 
  
 rem Getting the sources
-echo  "Fetching sources from SVN..." 
+echo  Fetching sources from SVN... 
 
 set URL=https://mdsp.googlecode.com/svn/trunk/
 svn checkout %URL% mdsp 2>&1
 
 rem Build funcsim
-devenv mdsp/sources/funcsim/funcsim.vcproj /build "Release|Win32" /out %LOGFILE% 
+devenv mdsp/sources/funcsim/funcsim.vcproj /useenv /build "Release|Win32" 
+if errorlevel 1 goto :builderror
 
-echo That's all for now
+rem Run unit tests
+echo Unit tests are not implemented yet
+REM mdsp\sources\funcsim\Release\test.exe
+REM if errorlevel 1 goto :unittesterror
 
-goto :endbuild
+rem Run funcsim
+echo Running funcsim
+mdsp\sources\funcsim\Release\test.exe mdsp\tests\simple_test.bin
+if errorlevel 1 goto :funcsimerror
 
-rem the stuff below is not implemented yet
-rem --------------------------------------------------------------------------------
-if [ -n "$BRANCHNAME" ]
-then
-    URL="https://mdsp.googlecode.com/svn/branches/$BRANCHNAME"
-else
-    URL="https://mdsp.googlecode.com/svn/trunk/"
-fi
-SVNOUTPUT=`svn checkout $URL mdsp 2>&1`
-SVNERRORCODE=$?
-if [ $SVNERRORCODE != 0 ] # Failure
-then
-    logprint "Failed with code "$SVNERRORCODE". The output is:"  
-    logprint "$SVNOUTPUT"  
-    logprint "No more actions were done"  
-    mail_results "$LOGFILE"
-    # TODO bail out
-    exit 1
-else # Success
-    logprint "OK."  
-  # Get svn revision
-    REVISION=`svnversion mdsp`
-    logprint "SVN Revision is $REVISION"
-fi
+rem Success
+set PASS=1
+echo =====================================================
+echo Tests passed!
+goto :mailresults
+   
  
-# Let' build the code
-MAKEDIR="mdsp"
-logprint "Building..."  
-MAKEOUTPUT=`make -C $MAKEDIR $MAKEFLAGS 2>&1`
-MAKEERRORCODE=$?
-if [ $MAKEERRORCODE != 0 ] # Failure
-then
-    logprint "Failed with code $MAKEERRORCODE.The output is:"  
-    logprint "$MAKEOUTPUT"  
-    logprint "No more actions were done"   
-    mail_results "$LOGFILE"
- 
-    # TODO bail out
-    exit 1
-else # Success
-    logprint "OK."  
-    # Check if there were any warnings
-    if [[ $MAKEOUTPUT  == *warning\:* ]]
-    then
-      logprint "There were warnings during build"
-      logprint "$MAKEOUTPUT"
-    fi
-fi
- 
-# Let' run the test suite included in MDSP
-logprint "Running unit tests..."   
-TESTOUTPUT=`mdsp/Release/test 2>&1`
-TESTERRORCODE=$?
-if [ $TESTERRORCODE != 0 ] # Failure
-then
-    logprint "Failed with code $TESTERRORCODE. The output is:"  
-    logprint "$TESTOUTPUT"  
-    logprint "No more actions were done"  
-    mail_results "$LOGFILE"
-    # TODO bail out
-    exit 1
-else # Success
-    logprint "OK."  
-fi
- 
-# Let' run the additional tests
-logprint "Running product tests..."  
- 
-# TODO 
-# For each of lines specified
-# run the line, get its output
-# check error code 
-# add output to report
- 
-logprint "Not implemented yet"  
- 
-# Happily finish
-PASS=1
-logprint "All tests are OK."  
-ENDDATE=`date "+%Y-%m-%d-%H-%M-%S"`
-logprint "Finished at $ENDDATE"
-mail_results "$LOGFILE"
- 
-#cd "$HOME"
-logprint "Bye!"
-exit 0
- 
- 
+:builderror
+echo =====================================================
+echo Build error
+goto :mailresults
 
+:unittesterror
+echo =====================================================
+echo Unit test error
+goto :mailresults
 
-rem Exit point
-:endbuild
+:funcsimerror:
+echo =====================================================
+echo Funcsim error
+goto :mailresults
+
+rem Exit point -------------------------------------------
+:mailresults
+
+cd C:\buildbot
+set MAILADDRESS="multimedia-dsp-2010@googlegroups.com"
+rem set MAILADDRESS="ggg_mail@inbox.ru"
+set MAILNAME="mdsp.buildbot@inbox.ru"
+
+rem Read password for mail
+for /f %%x in (C:\cygwin\bin\cat.exe .mdsp_bot_passwd ) do set MAILPASS=%%x
+
+rem See crontab to understand how testlog.txt is created
+set MFILE=testlog.txt
+
+set MDATE=%DATE%
+
+set SUBJ=[FAIL]
+if %PASS%==1 set SUBJ=[PASS]   
+
+set SUBJ=%SUBJ% MDSP testing results (%MDATE%)
+
+sendemail.exe -f %MAILNAME% -t %MAILADDRESS%  -xu %MAILNAME%  -xp %MAILPASS% -u %SUBJ% -s smtp.inbox.ru:25 -o message-file=%MFILE% -o message-charset=cp866
+echo The mail is sent at %MDATE% to %MAILADDRESS% 1>&2
+
