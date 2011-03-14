8523fceb5f162e5467e1e2d59aa05fc7829e2361
kryukov@frtk.ru <kryukov@frtk.ru@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2011-03-14 11:24:39 +0000

    [branch] Queue model of port is implemented. New demonstration of ports.
    git-svn-id: https://mdsp.googlecode.com/svn/branches@535 77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7

--- kryukov-ports/sources/funcsim/demoports.cpp
+++ kryukov-ports/sources/funcsim/demoports.cpp
@@ -1,12 +1,20 @@
 /**
  * demoports.cpp
  *
- * Very simple test of ports. Connects WritePort with one ReadPort with defined latency (10)
- * We are adding data to port in 0 clock, and trying to get it in all clocks.
- * But we can read it only on 10 clock because latency of ReadPort is 10.
- * On the eleventh clock program failed, because there's no data in port.
+ * Very simple demos of ports. 
  *
- * Kryukov Pavel
+ * First demo:
+ * Connects WritePort with one ReadPort with defined latency (10)
+ * We are adding data to port in 3 and 4 clock, and trying to get it in all clocks.
+ * But we can read it only on 13 and 14 clock because latency of ReadPort.
+ *
+ * 
+ * Second demo:
+ * Connects WritePort and two ReadPorts with different latency.
+ * We are adding data to port in 3 and 4 clock, trying to get in from Reader1 from 5 to 13 clock.
+ * After, from 14 to 19 clock we're trying to read data from Reader2.
+ *
+ * Kryukov Pavel, 2011
 */
 
 #include "ports.h"
@@ -16,21 +24,56 @@ static int SIZE = 100;
 int main(int argc, char* argv[])
 {
     WritePort<int> writer("INIT", SIZE);
-    ReadPort<int> reader("INIT", 10);
+    ReadPort<int> reader1("INIT", 10);
+    ReadPort<int> reader2("INIT", 15);
     
-    int buffer = 5;
+    int operation1 = 100;
+    int operation2 = 200;
     int *address;
+   
+    std::cout << std::endl << "First demo" << std::endl;
     
-    writer.write(&buffer, 0);
+    writer.write(&operation1, 3);
+    std::cout << "'" << operation1 << "' was added on 3-rd clock" << std::endl;
     
-    for ( unsigned i = 1; i < SIZE; i++)
+    writer.write(&operation2, 4); 
+    std::cout << "'" << operation2 << "' was added on 4-rd clock" << std::endl;    
+    
+    for ( unsigned i = 5; i < 15; i++)
     {
         std::cout << i << "-th clock;" << std::endl;
-        if (!reader.read(&address, i))
+        if (!reader1.read(&address, i))
         {
-            std::cout << *address << " was readed on " << i << "-th clock" << std::endl;
+            std::cout  << "'" << *address << "' was readed from ReadPort1 on " << i << "-th clock" << std::endl;
         }
     }
+    
+    std::cout << std::endl << "Second demo" << std::endl;
+    
+    writer.write(&operation1, 3);
+    std::cout << "'" << operation1 << "' was added on 3-rd clock" << std::endl;
+    
+    writer.write(&operation2, 4); 
+    std::cout << "'" << operation2 << "' was added on 4-rd clock" << std::endl;  
+    
+    for ( unsigned i = 5; i < 14; i++)
+    {
+        std::cout << i << "-th clock;" << std::endl;
+        if (!reader1.read(&address, i))
+        {
+            std::cout  << "'" << *address << "' was readed from ReadPort1 on " << i << "-th clock" << std::endl;
+        }
+    }
+    
+    for ( unsigned i = 14; i < 20; i++)
+    {
+        std::cout << i << "-th clock;" << std::endl;
+        if (!reader2.read(&address, i))
+        {
+            std::cout  << "'" << *address << "' was readed from ReadPort2 on " << i << "-th clock" << std::endl;
+        }
+    }    
+    
     
     return 0;
 
