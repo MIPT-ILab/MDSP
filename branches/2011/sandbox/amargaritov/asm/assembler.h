f667b98ef28486bc82d7d702a8a968b4c66cc34c
artemy.margaritov <artemy.margaritov@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2011-11-21 19:26:39 +0000

    [branch] create "amargaritov" branch folder in the sandbox and copy there the "sources" folder

---
+++ branches/2011/sandbox/amargaritov/asm/assembler.h
@@ -0,0 +1,68 @@
+/** @file assembler.h
+ * Definition of Assembler class, the assembler output generator
+ * @author Alexander Potashev
+ * Copyright 2009 MDSP team
+ */
+
+#include <vector>
+
+#include "memory.h"
+#include "semantican.h"
+
+
+/**
+ * This class is used to assemble an executable image a sequence of
+ * semantic units
+ */
+class Assembler
+{
+public:
+    /**
+     * Convenience constructor
+     *
+     * @param units The sequence of semantic units to work with.
+     */
+    Assembler( std::vector<SemanticUnit *> units);
+
+    /**
+     * dtor
+     */
+    ~Assembler();
+
+    /**
+     * Generates an executable image defined by a list of
+     * pairs (byte address; byte value)
+     */
+    std::map<unsigned int, hostUInt8> run();
+
+    /**
+     * Encodes a processor instruction located at word address @p pc
+     * in program memory
+     *
+     * @param operation Pointer to the assembler command.
+     * @param pc The word address of the instruction in program memory.
+     */
+    ByteLine *encodeOperation(
+        SemanticUnit *operation, unsigned int pc);
+
+private:
+    /**
+     * @internal
+     * Return the number of a register define by an identifier, e.g.
+     * "%r23" => 23
+     */
+    int getGprNum( std::string id);
+
+private:
+    /**
+     * The sequence of semantic units given on the input
+     */
+    std::vector<SemanticUnit *> units;
+
+    /**
+     * map from label identifiers to its absolute byte addresses
+     * in program memory
+     */
+    std::map<std::string, unsigned int> label_addr;
+};
+
