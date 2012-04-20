76427437d94ba14b50667b4a1ed30cce7dcda941
denis.evlanov@gmail.com <denis.evlanov@gmail.com@77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7>
2012-04-20 21:20:21 +0000

    [branch] create very very very unstable cashe that doesn't work
    git-svn-id: https://mdsp.googlecode.com/svn/branches/2011@756 77eb06f4-9e05-11de-bf4a-bd5be0e2b6d7

--- sandbox/devlanov_cashe/cashe.cpp
+++ sandbox/devlanov_cashe/cashe.cpp
@@ -37,31 +37,31 @@ Cashe::Cashe( unsigned int sizeParam, unsigned int waysParam, unsigned int block
     }
     //cout << "xasxaxasx" << endl;
     mask = mask << bit_in_cashe_index;
-    cout << "MASK = " << mask << endl;
+    //cout << "MASK = " << mask << endl;
     mask = ~mask;
     //mask = ~mask;
     //cout << "MASK = " << mask << endl;
-    cout << "number_sets_priv = " << number_sets_priv << endl;
+    //cout << "number_sets_priv = " << number_sets_priv << endl;
     //cout << "( cash_array[ i + k])->cash_index = " << ( cash_array[ 0])->cash_index << endl;
     long int i = 0;
     
     while ( i != number_sets_priv * ways)
     {
-       	cout << "i = " << i << " ";
+       	//cout << "i = " << i << " ";
        	long int k = 0;
        	for ( k = 0; k < ways; k++)
        	{
        		( cash_array[ i + k])->cash_index = i & mask;////cout << "helohelo" << endl;
        		( cash_array[ i + k])->activ_address = 0;
        		( cash_array[ i + k])->there_is = false;
-       		( cash_array[ i + k])->number_change_activ_addr = 0;cout << "k = " << k << " ";
+       		( cash_array[ i + k])->number_change_activ_addr = 0;//cout << "k = " << k << " ";
 		}
-		cout << "number_sets_priv * ways = " << number_sets_priv * ways << endl;
-		cout << endl;
+		//cout << "number_sets_priv * ways = " << number_sets_priv * ways << endl;
+		//cout << endl;
 		i = i + ways;
 		
 	}
-	cout << "helohelo" << endl;
+	//cout << "helohelo" << endl;
 }
 
 void Cashe::processRead( unsigned int addr)
